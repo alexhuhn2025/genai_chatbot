a5ef5afb8ef2511484f795766eaec9faf184d8a2
alexhuhn2025 <huhn.alex@yahoo.com>
2025-12-19 21:48:39 +0000

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -5,13 +5,7 @@ from langchain_groq import ChatGroq
 # load the env variables
 load_dotenv()   # path to where .env file is located. leave blank if in same directory. do not need to run os.environ[]=...
 
-
-# os.environ["GROQ_API_KEY"] = GROK_API_KEY 
 # set up streamlit
-# run with:
-#      python -m streamlit run .\chatbot.py
-# to open again after accidentally closing window, goto new browser and search in line the localURL link,
-#       localhost:8501
 st.set_page_config(
     page_title="˚.🎀༘⋆ Generative AI Chatbot, hmmm",
     page_icon = "⸜(｡˃ ᵕ ˂ )⸝♡", # emojhttps://emojidb.org/cute-japanese-emojis
@@ -21,14 +15,8 @@ st.set_page_config(
 st.title("Generative AI CHatbot, ask me a question")
 
 # initiate chat history
-# chat_history = [] # after every user interaction, streamlit re-reads the entire code again, and this varaible will get re-initiated, thus not able to keep record of chat history, need session_state
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
-
-# session_state is a dictionary that holds the variable name chat_history as a Key, ie. The variables in this dictionary are not rendered again after user interaction -- maintains memory
-# {
-#     "chat_history":[]
-# }
 
  # show the chat history
 for message in st.session_state.chat_history:
@@ -55,3 +43,4 @@ if user_prompt:
     with st.chat_message('assistant'):
 
         st.markdown(assistant_response)
+
